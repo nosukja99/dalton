6124cd5b0049c0f0dee8ed278167bce752a916c6
romielisse <romielisse@gmail.com>
2018-07-11 13:39:10 -0400

    sql insert updates for student-class

---
+++ misc/melissa.sql
@@ -0,0 +1,27 @@
+use dalton;
+
+select * from student;
+select * from class;
+select * from classroom;
+select * from course;
+select * from subject;
+select * from major;
+select * from instructor;
+select * from department;
+
+-- list of classes and instructors
+select course.id as course_id, course.course_name, class.crn, class.day, class.time, instructor.instructor_name
+from course
+inner join class on course.id = class.course_id
+inner join instructor on instructor.id = class.instructor_id
+order by instructor_name;
+
+-- list of classes and students in each class
+select course.course_name, class.crn, instructor.instructor_name, student.id as student_id, student.class_id as class_id, student.student_name
+from course
+inner join class on course.id = class.course_id
+inner join instructor on instructor.id = class.instructor_id
+inner join student on class.id = student.class_id
+order by course.course_name, crn;
+
+update student set class_id=null
